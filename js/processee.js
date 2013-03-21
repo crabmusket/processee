f16333c59090b6a679bee651c19597463c7cd93a
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-03-21 22:55:34 +1100

    'do' replaces 'with' and implements the stack.
    This way there's less awkward syntax to deal with, but we still
    get the bonus of stacked (and inherited) states. It also trivially
    handles the case of defining external functions to pass to a @do
    block.

--- js/processee.js
+++ js/processee.js
@@ -1,30 +1,89 @@
+function rgb(r, g, b) {
+	return {
+		mode: 'rgb',
+		x: r, y: g, z: b,
+	};
+}
+
+function gray(v) {
+	return {
+		mode: 'rgb',
+		x: v, y: v, z: v,
+	};
+}
+
+function hsv(h, s, v) {
+	return {
+		mode: 'hsv',
+		x: h, y: s, z: v,
+	};
+}
+
 function processee(fn) {
 	return function(p) {
 		p.make = {
-			rect: function(x, y, w, h) {
-				if(typeof x == "object") {
-					if(typeof x.pos == "object") {
-						x.x = x.pos.x || x.x;
-						x.y = x.pos.y || x.y;
-					}
-					p.rect(x.x || x.posX,
-						   x.y || x.posY,
-						   x.w || x.width,
-						   x.h || x.height);
-				} else {
-					p.rect(x, y, w, h);
-				}
+			rect: function(r) {
+				p.rect(r.x || r.posX,
+					   r.y || r.posY,
+					   r.w || r.width,
+					   r.h || r.height);
 			},
 		};
+
+		p.set = {};
+		p.set.__defineSetter__("fill", function(c) {
+			if(!c.mode) {
+				console.log('Cannot fill without a mode. Given:', c);
+				return;
+			}
+			p.__stack[p.__stack.length-1].mode = c.mode == 'hsv' ? p.HSV : p.RGB;
+			p.__stack[p.__stack.length-1].fill = {x: c.x, y: c.y, z: c.z};
+			p.__stackSet();
+		});
+
+		p.set.__defineSetter__("stroke", function(c) {
+			if(!c.mode) {
+				console.log('Cannot stroke without a mode. Given:', c);
+				return;
+			}
+			p.__stack[p.__stack.length-1].stroke = {x: c.x, y: c.y, z: c.z};
+			p.__stackSet();
+		});
+
+		p.__stackSet = function() {
+			var c = p.__stack[p.__stack.length-1];
+			p.colorMode(c.mode);
+			p.fill(c.fill.x, c.fill.y, c.fill.z);
+			p.stroke(c.stroke.x, c.stroke.y, c.stroke.z);
+		};
+		p.__stackPush = function() {
+			var c = p.__stack[p.__stack.length-1];
+			p.__stack.push({
+				mode: c.mode,
+				fill: c.fill,
+				stroke: c.stroke,
+			});
+		};
+		p.__stackPop = function() {
+			p.__stack.pop();
+			p.__stackSet();
+		};
+
 		p.reset = function() {
-			p.fill(255);
-			p.stroke(0);
+			p.__stack = [{
+				mode: p.RGB,
+				fill: { x: 255, y: 255, z: 255 },
+				stroke: { x: 0, y: 0, z: 0 },
+			}];
+			p.__stackSet();
 		};
-		p.with = function(set, fn) {
-			set.call(p);
+
+		p.do = function(fn) {
+			p.__stackPush();
 			fn.call(p);
-			p.reset();
+			p.__stackPop();
 		};
+
 		p.setup = function() {
 			p.reset();
 		};

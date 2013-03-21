69af888648a8473f7d7d9c4d4aeaf176885072fe
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-03-22 09:37:22 +1100

    Added ellipse and circle drawers.

--- js/processee.js
+++ js/processee.js
@@ -32,6 +32,28 @@ function processee(fn) {
 			}
 		};
 
+		p.drawEllipse = function(x, y, w, h) {
+			if(typeof x == 'object') {
+				p.ellipse(x.x || x.posX,
+				          x.y || x.posY || 0,
+				          x.w || x.width || 0,
+				          x.h || x.height);
+			} else {
+				p.ellipse(x, y, w, h);
+			}
+		};
+
+		p.drawCircle = function(x, y, r) {
+			if(typeof x == 'object') {
+				p.ellipse(x.x || x.posX || 0,
+				          x.y || x.posY || 0,
+				          x.r || x.radius,
+				          x.r || x.radius);
+			} else {
+				p.ellipse(x, y, r, r);
+			}
+		};
+
 		p.__defineSetter__("fillColor", function(c) {
 			if(!c.mode) {
 				console.log('Cannot set fill without a color mode. Given:', c);

1ada18d3a6a1b6a556b870dbc99a5dabfe505df2
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-03-29 21:22:11 +1100

    Moved processee object into processee.js.

--- js/processee.js
+++ js/processee.js
@@ -1,3 +1,40 @@
+window.processee = {
+	procedures: [],
+	setups: [],
+
+	do: function(fn) {
+		window.processee.procedures.push({
+			layer: 1,
+			procedure: fn,
+		});
+	},
+
+	setup: function(fn) {
+		window.processee.setups.push(fn);
+	},
+
+	doOnLayer: function(layer, fn) {
+		window.processee.procedures.push({
+			layer: layer,
+			procedure: fn,
+		});
+	},
+
+	run: function() {
+		$('#output').toggle(true);
+		eval(CoffeeScript.compile(window.cm.getValue()));
+		window.processee.procedures.sort(function(a, b) { return a.layer - b.layer; });
+		if(window.processingInstance) window.processingInstance.exit();
+		window.processingInstance = new Processing($('#processing')[0],
+			window.processee.create());
+	},
+
+	stop: function() {
+		$('#output').toggle(false);
+		if(window.processingInstance) window.processingInstance.exit();
+	},
+};
+
 function rgb(r, g, b) {
 	return {
 		mode: 'rgb',
@@ -464,3 +501,4 @@ window.processee.create = function() {
 		};
 	}
 };
+
